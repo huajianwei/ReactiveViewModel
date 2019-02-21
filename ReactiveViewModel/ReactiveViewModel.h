98009a9dad98445c34177f761ee99621efd09cd2
phoneix <huajianwei@users.noreply.github.com>
2019-02-21 23:01:33 +0800

    Update ReactiveViewModel.h

--- ReactiveViewModel/ReactiveViewModel.h
+++ ReactiveViewModel/ReactiveViewModel.h
@@ -16,8 +16,4 @@ FOUNDATION_EXPORT const unsigned char ReactiveViewModelVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ReactiveViewModel/PublicHeader.h>
 
-
-
-
-
 #import <ReactiveViewModel/RVMViewModel.h>
